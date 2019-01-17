a0ad4303c3151b119df886619aca807d71681826
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-17 12:11:44 +0100

    Refactor github repo info converter function

--- src/github/converter.js
+++ src/github/converter.js
@@ -1,6 +1,5 @@
 function converter(repoInfo) {
-  const { name, stargazers_count: stars, license: licenseFull } = repoInfo;
-  const { spdx_id: license = null } = licenseFull;
+  const { name, stargazers_count: stars, license: { spdx_id: license = null } } = repoInfo;
   return {
     name,
     stars,
