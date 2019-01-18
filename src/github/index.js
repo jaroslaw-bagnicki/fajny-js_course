170e9aab57cae1b88fad092a468023d6b968b048
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-18 22:33:52 +0100

    Convert 'getRepos' fn to async/await

--- src/github/index.js
+++ src/github/index.js
@@ -1,7 +1,8 @@
 import fetchRepos from './service';
 
-function getRepos() {
-  fetchRepos().then(arr => arr.forEach(repo => console.log(repo.toString())));
+async function getRepos() {
+  const arr = await fetchRepos();
+  arr.forEach(repo => console.log(repo.toString()));
 }
 
 export default getRepos;
