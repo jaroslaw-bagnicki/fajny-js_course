75450a0aabbcc2a82eac1eebd227b6cfdd389693
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-18 22:18:27 +0100

    Add transform repo data in gh-service

--- src/github/service.js
+++ src/github/service.js
@@ -1,6 +1,17 @@
+import { GitHubRepo } from './model';
+
 /* global fetch */
 const URL = 'https://api.github.com/users/jaroslaw-bagnicki/repos';
 const FORBIDEN_REPOS = ['kurs-gita', 'Mosh-11', 'webpack_demo', 'parcel_demo'];
+
+function converter(repoInfo) {
+  const { name, stargazers_count: stars, license } = repoInfo;
+  return new GitHubRepo({
+    name,
+    stars,
+    license: license ? license.spdx_id : ''
+  });
+}
 
 function fetchRepos() {
   return fetch(URL)
@@ -10,8 +21,10 @@ function fetchRepos() {
       }
       throw Error('Response not 200');
     })
-    .catch(err => console.warn(err))
-    .then(arr => arr.filter(repo => !FORBIDEN_REPOS.includes(repo.name)));
+    .then(arr => arr
+      .filter(repo => !FORBIDEN_REPOS.includes(repo.name))
+      .map(converter))
+    .catch(err => console.warn(err));
 }
 
 export default fetchRepos;
