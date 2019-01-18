a1bf9004a50f5efffc871f303cc2665b7a1a1917
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-18 22:11:37 +0100

    Add filter to 'getRepo' function

--- src/github/service.js
+++ src/github/service.js
@@ -1,5 +1,6 @@
 /* global fetch */
 const URL = 'https://api.github.com/users/jaroslaw-bagnicki/repos';
+const FORBIDEN_REPOS = ['kurs-gita', 'Mosh-11', 'webpack_demo', 'parcel_demo'];
 
 function fetchRepos() {
   return fetch(URL)
@@ -9,7 +10,8 @@ function fetchRepos() {
       }
       throw Error('Response not 200');
     })
-    .catch(err => console.warn(err));
+    .catch(err => console.warn(err))
+    .then(arr => arr.filter(repo => !FORBIDEN_REPOS.includes(repo.name)));
 }
 
 export default fetchRepos;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-/* global alert */
-
 // import game from './game';
-import ghConverter from './github/converter';
 import main from './github';
 
 main();
