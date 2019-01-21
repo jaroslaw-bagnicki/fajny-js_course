ae2a6b7734f2cdab3a8dfb780301150416a4a15a
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-21 12:51:44 +0100

    Refactor gh-service

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@
 // import main from './github';
 // import showJoke from './joke';
 import 'markdown-element';
+import { getPost, getRepos } from './github';
 
-// main();
 // showJoke();
+
+async function logRepos() {
+  const arr = await getRepos();
+  arr.forEach(repo => console.log(repo.toString()));
+}
+// logRepos();
+
+async function renderPost(fileName) {
+  const markdown = await getPost(fileName);
+  const post = document.createElement('mark-down');
+  post.textContent = markdown;
+  return post;
+}
+
+renderPost('1.md').then(post => document.body.appendChild(post));
