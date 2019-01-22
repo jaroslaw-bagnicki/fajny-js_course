8e9a1b331d77581219edcb34516799be50261f7e
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-22 06:59:18 +0100

    Config webpack: add CopyWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new CopyWebpackPlugin({
+      from: 'src/assets/',
+      to: 'dist/assets/'
     })
   ]
-}
+};
