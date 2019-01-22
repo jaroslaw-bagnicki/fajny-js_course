d0dd3232ca419f0bdb8a581465c0f780779a310b
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-22 08:21:09 +0100

    Webpack config: add CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
