49171e96d109ad4fa59781934eecf3a4a569e859
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-23 08:00:45 +0100

    Bind game & joke to UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import scss from './styles/main.scss';
-// import game from './game';
-// import main from './github';
-// import './joke';
+import startGame from './game';
+import showJoke from './joke';
 import './blog';
 import './about-me';
+
+// UI vars
+const UIGameBtn = document.querySelector('.button.game');
+const UIJokeBtn = document.querySelector('.button.joke');
+const UIBlogBtn = document.querySelector('.button.blog');
+
+// Add event listerner
+UIGameBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  startGame();
+});
+
+UIJokeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  showJoke();
+});
