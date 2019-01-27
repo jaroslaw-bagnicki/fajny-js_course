fb7ea0cbfb9426bc90b6c726eb80494849371af4
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-27 11:43:34 +0100

    Configure separte modules bundles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './styles/main.scss';
 import startGame from './game';
 import showJoke from './joke';
-import './blog';
 import './about-me';
+
+console.log('Main module loaded');
 
 // UI vars
 const UIGameBtn = document.querySelector('.button.game');
 const UIJokeBtn = document.querySelector('.button.joke');
-const UIBlogBtn = document.querySelector('.button.blog');
 
 // Add event listerner
 UIGameBtn.addEventListener('click', (e) => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  entry: {
+    main: './src/index.js',
+    blog: './src/blog/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'src/index.html',
+      chunks: ['main']
     }),
     new HtmlWebpackPlugin({
       template: 'src/blog/index.html',
-      filename: 'blog/index.html'
+      filename: 'blog.html',
+      chunks: ['blog']
     }),
     new CopyWebpackPlugin([{
       from: 'src/assets/',
