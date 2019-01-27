b6a7fdd83406bd5cd4d4fddce402a345a3b7035f
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-27 05:23:20 +0100

    Add blog page file

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
+    new HtmlWebpackPlugin({
+      template: 'src/blog/index.html',
+      filename: 'blog/index.html'
+    }),
     new CopyWebpackPlugin([{
       from: 'src/assets/',
       to: 'assets/'
