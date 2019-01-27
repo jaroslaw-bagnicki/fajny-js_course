75e8ae5244e7d81dedf9277d1ec391e70f43d1fe
Jarosław Bagnicki <jarek@bagnicki.net>
2019-01-27 20:51:36 +0100

    Add blog footer component

--- src/blog/components/index.js
+++ src/blog/components/index.js
@@ -1,8 +1,10 @@
 import { Avatar } from './Avatar';
 import { BlogHeader } from './BlogHeader';
 import { BlogPost } from './BlogPost';
+import { BlogFooter } from './BlogFooter';
 
 // Registering custom elements
 customElements.define('avatar-img', Avatar);
 customElements.define('blog-header', BlogHeader);
 customElements.define('blog-post', BlogPost);
+customElements.define('blog-footer', BlogFooter);
